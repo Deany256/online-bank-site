68779ed088f6c3294b9d6156f3d2548e12ecad2a
Deany256 <gdeandude@gmail.com>
2024-10-22 21:01:36 +0100

    Update routes.py

--- app/transactions/routes.py
+++ app/transactions/routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, abort
 from flask_login import login_required, current_user
 from app.models import Account, Transaction
 
