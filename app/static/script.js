374c0e949d72dec4a98c1caefef5a121eb9e7ab3
Deany256 <gdeandude@gmail.com>
2024-11-26 18:16:59 +0000

    Create script.js

---
+++ app/static/script.js
@@ -0,0 +1,10 @@
+// document.addEventListener("DOMContentLoaded", function () {
+//     const authLink = document.getElementById("auth-link");
+//     const isAuthenticated = {{ current_user.is_authenticated | tojson }};
+    
+//     if (isAuthenticated) {
+//         authLink.innerHTML = `<a href="/dashboard">Welcome, {{ current_user.username }}</a>`;
+//     } else {
+//         authLink.innerHTML = `<a href="/login">Login/Signup</a>`;
+//     }
+// });
