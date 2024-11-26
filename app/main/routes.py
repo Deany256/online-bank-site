0692cf04949a605b28cbf6b4c62963c98ece3b66
Deany256 <gdeandude@gmail.com>
2024-11-26 18:16:57 +0000

    Update routes.py

--- app/main/routes.py
+++ app/main/routes.py
@@ -1,9 +1,11 @@
-from flask import Blueprint, render_template
-
+from flask import Blueprint, render_template, url_for, redirect
+from flask_login import current_user, login_required
 # Create a blueprint for the main routes
 main_bp = Blueprint('main', __name__)
 
 # Home route to render the base.html template
 @main_bp.route('/')
 def home():
+    if current_user.is_authenticated:
+        return redirect(url_for('accounts.dashboard'))
     return render_template('base.html')
